629179ffc033f95a2b7c8632dc6ee10242b65911
Toriola <faaedah@gmail.com>
2023-12-29 08:31:34 +0100

    ADD: added measurement

--- src/components/Measurement.jsx
+++ src/components/Measurement.jsx
@@ -1,10 +1,38 @@
-export default function Measurement () {
-    return (<>
-        <p>
-            <span>Sl</span>
-        <span></span></p>
-
-        <p>Sl</p>
-        <p>33</p>
-    </>)
+export default function Measurement() {
+   return (
+      <div className="flex flex-wrap gap-y-10 gap-16 measurement-details">
+         <p>
+            <span>Lenght</span>
+            <span>12/37</span>
+         </p>
+         <p>
+            <span>Band</span>
+            <span>12/37</span>
+         </p>
+         <p>
+            <span>Thigh</span>
+            <span>12/37</span>
+         </p>
+         <p>
+            <span>Sleeve</span>
+            <span>12/23/45</span>
+         </p>
+         <p>
+            <span>Cuff</span>
+            <span>12/37</span>
+         </p>
+         <p>
+            <span>Agbada</span>
+            <span>37</span>
+         </p>
+         <p>
+            <span>Trouser</span>
+            <span>12</span>
+         </p>
+         <p>
+            <span>L</span>
+            <span>12/37</span>
+         </p>
+      </div>
+   );
 }
